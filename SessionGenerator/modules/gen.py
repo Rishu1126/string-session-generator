491fe2ab9875a533dcbb01796f6e952cf5b886cc
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-07-13 14:39:18 +0530

    Update gen.py

--- SessionGenerator/modules/gen.py
+++ SessionGenerator/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@ultroidofficial_chat"))
+            await client(JoinChannelRequest("@bot_hub_network"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
