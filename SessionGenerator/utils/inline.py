3f0f780f1cbba513df053640c45c5576dc7c25b8
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-07-13 14:09:38 +0530

    Update inline.py

--- SessionGenerator/utils/inline.py
+++ SessionGenerator/utils/inline.py
@@ -7,11 +7,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="❈ 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 ❈", callback_data="gensession")],
+        [InlineKeyboardButton(text="💓 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲 𝗦𝗲𝘀𝘀𝗶𝗼𝗻 💓", callback_data="gensession")],
         [
-            InlineKeyboardButton(text="❈ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❈", url="https://t.me/SUPPORT_CHAT"),
+            InlineKeyboardButton(text="💘 𝗢𝗪𝗡𝗘𝗥 💘", url="https://t.me/Abhi_rss"),
             InlineKeyboardButton(
-                text="❈ Channel ❈", url="https://t.me/Ultroid_official"
+                text="🥀𝗕𝗢𝗧𝗛𝗨𝗕 𝗡𝗘𝗧𝗪𝗢𝗥𝗞🥀", url="https://t.me/bot_hub_network"
             ),
         ],
     ]
@@ -20,13 +20,13 @@ keyboard = InlineKeyboardMarkup(
 gen_key = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="❣️ 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯1 ❣️", callback_data="pyrogram1"),
-            InlineKeyboardButton(text="🦋 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯2 🦋", callback_data="pyrogram"),
+            InlineKeyboardButton(text="💥 𝗣𝗬𝗥𝗢𝗚𝗥𝗔𝗠 𝗩𝟭 💥", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="🍁 𝗣𝗬𝗥𝗢𝗚𝗥𝗔𝗠 𝗩𝟮 🍁", callback_data="pyrogram"),
         ],
-        [InlineKeyboardButton(text="🌼 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 🌼", callback_data="telethon")],
+        [InlineKeyboardButton(text="⚡ 𝗧𝗘𝗟𝗘𝗧𝗛𝗢𝗡 ⚡", callback_data="telethon")],
     ]
 )
 
 retry_key = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text="⚡️ 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 ⚡️", callback_data="gensession")]]
+    [[InlineKeyboardButton(text="🌜 𝗧𝗿𝘆 𝗔𝗴𝗮𝗶𝗻 🌛", callback_data="gensession")]]
 )
