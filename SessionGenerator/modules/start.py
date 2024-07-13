57e50c52de9c9ffa1220eb2e91c365179c994dcc
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-07-13 14:12:31 +0530

    Update start.py

--- SessionGenerator/modules/start.py
+++ SessionGenerator/modules/start.py
@@ -10,7 +10,7 @@ from SessionGenerator.utils import add_served_user, keyboard
 @Opleech.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"🦋 Hey {message.from_user.first_name},\n\n⌘ This is {Opleech.mention},\nAn open source string session generator bot, written in python with the help of pyrogram ].",
+        text=f"😚 𝗛𝗘𝗬 𝗕𝗔𝗕𝗬 {message.from_user.first_name},\n\n🥀 𝗧𝗵𝗶𝘀 𝗜𝘀 {Opleech.mention},\n𝗔𝗻 𝗢𝗽𝗲𝗻 𝗦𝗼𝘂𝗿𝗰𝗲 𝗦𝘁𝗿𝗶𝗻𝗴 𝗦𝗲𝘀𝘀𝗶𝗼𝗻 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗼𝗿 𝗕𝗼𝘁, 𝗪𝗿𝗶𝘁𝘁𝗲𝗻 𝗜𝗻 𝗣𝘆𝘁𝗵𝗼𝗻 ❤].",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
