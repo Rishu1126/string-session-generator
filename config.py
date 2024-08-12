f645f1f221a25ec7edc50f800ce0e5144348a8d3
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-08-13 00:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("27098166"))
+API_HASH = getenv("962e823666b17f1c4fe94d9b200284a8")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("6743171827:AAFyMG2xtxhvXYQliq7Yf0EVgigFYSPSyKM")
+OWNER_ID = int(getenv("5516578116"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/bot_hub_network")
+MONGO_DB_URI = getenv("mongodb+srv://vocon90583:Rishabh1126@cluster0.qyin9uj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/trending_era")
